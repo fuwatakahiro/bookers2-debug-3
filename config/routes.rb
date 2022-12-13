8139bc4e770032627b91ed9b4294dc6d1a3f2292
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-12-13 09:35:00 +0000

    dm

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   end
   get "seaches" => "seaches#seach"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :chats, only: [:show, :create]
 end
