f2c1bb4a901a192990169aca6530da30418f4eb2
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-12-20 09:35:35 +0000

    [update]グループ機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,11 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+  def join
+    @group = Group.find(params[:group_id])
+    @group.users << current_user
+    redirect_to groups_path
+  end
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
@@ -33,6 +38,11 @@ class GroupsController < ApplicationController
     else
       render :edit
     end
+  end
+  def destroy
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user)
+    redirect_to groups_path
   end
 
   private
