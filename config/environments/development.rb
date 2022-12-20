c99900fd1a65a9df89b3cac70fc10270cb263c8c
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-12-20 23:28:37 +0000

    [finish]グループメール

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,16 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 config.hosts << "c5e17de14f6f4a939f02db49fc9e5daa.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            ENV['KEY'],#Gmailのアドレス
+    password:             ENV['SECRET_KEY'],#後述！大苦戦しました！
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
